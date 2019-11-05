65fbe280155d57b92cdc28c4f08b36288d13392d
Skiwa <haegman.julien@laposte.net>
2019-11-05 16:46:28 +0100

    Project structure init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,32 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
 import {TodoService} from './todo.service';
 import {FormsModule} from '@angular/forms';
 import { QrCodeComponent } from './qr-code/qr-code.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
     TodoItemComponent,
-    QrCodeComponent
+    QrCodeComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [TodoService],
+  providers: [TodoService, AuthGuardService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
